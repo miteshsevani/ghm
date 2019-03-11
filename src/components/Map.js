e814786b21ec8140cc9ebe90ea31435a6f295ade
miteshsevani <msevani@gmail.com>
2019-03-11 16:30:45 +0000

    Update content

--- src/components/Map.js
+++ src/components/Map.js
@@ -16,7 +16,7 @@ export default class Map extends Component {
   render() {
     return(
       <StyledMap>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2484.319683802944!2d0.09209161600972171!3d51.48900077963211!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8af3351471dff%3A0xc115d86e5eabb446!2sGreenwich+Hindu+Temple!5e0!3m2!1sen!2suk!4v1551871774178" frameBorder="0" allowFullScreen></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2484.319683802944!2d0.09209161600972171!3d51.48900077963211!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8af3351471dff%3A0xc115d86e5eabb446!2sGreenwich+Hindu+Temple!5e0!3m2!1sen!2suk!4v1551871774178" frameBorder="0" allowFullScreen title="Grneewich Hindu Mandir"></iframe>
       </StyledMap>
     )
   }
