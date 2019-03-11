70706b40b46d49a579d615403dc66b9d830927f9
miteshsevani <msevani@gmail.com>
2019-03-11 16:24:42 +0000

    Styling updates

--- src/components/styleComponents/Body.js
+++ src/components/styleComponents/Body.js
@@ -3,8 +3,7 @@ import Styled from "styled-components";
 const Body = Styled.div`
   font-family: ${props => props.theme.fontFamily};
   font-size: ${props => props.theme.fontSize};  
-  color: ${props => props.theme.color};
-  background: ${props => props.theme.background};
+  color: ${props => props.theme.color};  
   line-height: ${props => props.theme.lineHeight};
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
