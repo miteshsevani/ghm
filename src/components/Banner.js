bb72295d5bdffc86d942b8b02d6dde2e7ad188a9
Mitesh Sevani <msevani@gmail.com>
2020-07-22 12:11:23 +0100

    Content updates

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,16 +2,17 @@ import React, { Component } from 'react';
 import Styled from 'styled-components';
 import ContentWrapper from './styleComponents/ContentWrapper';
 
-const StyledBanner = Styled.div`  
-  background-image: url('img/mandir.png');  
-  background-size: cover;  
+const StyledBanner = Styled.div`
+  background-image: url('img/temple-reopening.jpg');
+  background-size: contain;
   background-position: center;
+  background-repeat: no-repeat;
   width: 100%;
-  height: 300px;
+  height: 1200px;
 `;
 export default class Banner extends Component {
   render() {
-    return(      
+    return(
       <ContentWrapper>
         <StyledBanner />
       </ContentWrapper>
